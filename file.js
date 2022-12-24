9796c69e780014e0ec49225ca26e64c7859dab42
SneackyBillyy <bilel15113@gmail.com>
2022-12-24 15:12:25 +0100

    i finished

--- file.js
+++ file.js
@@ -1,7 +1,7 @@
 
 
 function getComputerChoice(){
-    let x=Math.floor(Math.random() * 3);
+    let x= Math.floor(Math.random() * 3);
     console.log(x);
     if (x == 0){
         return 'rock';
@@ -15,28 +15,28 @@ function getComputerChoice(){
 
 
 function playRound(playerSelection,computerSelection){
-    let test;
+    
       let playerselect = playerSelection.toUpperCase();
       if((playerselect =='ROCK' && computerSelection =='rock') || (playerselect =='SCISSORS' && computerSelection =='scissors') ||(playerselect =='PAPER' && computerSelection =='paper')) {
-                    return "it's a tie"
+                    return 2
       }else if(playerselect =='ROCK' && computerSelection =='scissors') {
-        test ==1;
-        return `You Win! ${playerselect} beats ${computerSelection}`
+        return 1;
+        
       }  else if(playerselect =='ROCK' && computerSelection =='paper') {
-        return test ==0;
-        return `You lose! ${computerSelection} beats ${playerselect}`
+        return 0;
+        
       }else if(playerselect =='PAPER' && computerSelection =='rock') {
-        return test == 1;
-        return `You Win! ${playerselect} beats ${computerSelection}`
+        return 1;
+       
       }else if(playerselect =='PAPER' && computerSelection =='scissors') {
-        return test ==0;
-        return `You lose! ${computerSelection} beats ${playerselect}`
+        return 0;
+        
       }else if(playerselect =='SCISSORS' && computerSelection =='paper') {
-        return test ==1;
-        return `You Win! ${playerselect} beats ${computerSelection}`
+        return 1;
+        
       }else if(playerselect =='SCISSORS' && computerSelection =='rock') {
-        return test ==0;
-        return `You lose! ${computerSelection} beats ${playerselect}`
+        return 0;
+        
       }
 }
 
@@ -47,13 +47,15 @@ function game(){
     for(let i=1;i<=5;i++){
         let playerSelection=prompt('choose between rock ,paper,scissors');
         let computerSelection = getComputerChoice();
-        playRound(playerSelection,computerSelection);
+        let test =playRound(playerSelection,computerSelection); 
         if(test ==1){
             playerWin++;
             console.log('the player win this round');
         }else if(test == 0){
             computerWin++;
             console.log('the computer win this round');
+        }else if(test ==2){
+            console.log('its a tie');
         }
     }
     if(playerWin<computerWin){
@@ -67,4 +69,4 @@ function game(){
 }
 
 
-console.log(game());
+game();
